aade59f8db07fc3ed4c71001a8e45dc759862f29
Glenn <61597338+GlennGoddard@users.noreply.github.com>
2022-06-06 09:21:39 -0700

    Update helpers.py

--- weatherflow2mqtt/helpers.py
+++ weatherflow2mqtt/helpers.py
@@ -718,7 +718,7 @@ class ConversionFunctions:
         if se >= 0:
             am = 1/(cos(radians(sz)) + 0.50572*pow((96.07995 - sz),(-1.6364)))
             si = (1353 * ((1-ah)*pow(.7, pow(am, 0.678))+ah))*(sin(radians(se)))
-        else
+        else:
             am = 1
             si = 0
         si = round(si)
